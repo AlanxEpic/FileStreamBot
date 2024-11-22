d7cde6a14e567461a4164bbe38625d82c7917435
AlanxEpic <108180019+AlanxEpic@users.noreply.github.com>
2024-11-22 15:39:02 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -13,15 +13,15 @@ class Telegram:
     UPDATES_CHANNEL = -1001784625175
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
-    FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
-    FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
+    FORCE_SUB = -1001784625175
+    FORCE_SUB = True
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
     FILE_PIC = env.get('FILE_PIC', "https://graph.org/file/5bb9935be0229adf98b73.jpg")
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
+    FLOG_CHANNEL = -1001784625175   # Logs channel for file logs
+    ULOG_CHANNEL = -1001784625175   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
